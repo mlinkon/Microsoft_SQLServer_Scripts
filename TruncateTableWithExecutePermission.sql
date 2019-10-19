3e1507d8a0199887c220a3793488b6d4985de96f
Linkon <mlinkon@users.noreply.github.com>
2019-10-19 19:01:10 +0530

    Update TruncateTableWithExecutePermission.sql

--- TruncateTableWithExecutePermission.sql
+++ TruncateTableWithExecutePermission.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE USP_TRUNCATE_TABLE
 (
   @TABLENAME NVARCHAR(100),
-  @ISDEBUG BIT =0,
+  @ISDEBUG BIT = 0,
   @SCHEMA_OWNER NVARCHAR(20) = NULL 
 )
 WITH EXECUTE AS OWNER
@@ -22,18 +22,18 @@ BEGIN
     
     BEGIN TRY 
            SET @sDBName = DB_NAME();
-           SELECT @objectName = @sDBName + 'dbo.' + Object_name(@@Proc_ID);
+           SELECT @objectName = @sDBName + 'dbo.' + OBJECT_NAME(@@ProcID);
            SET @errNumber = 0;
            SET @errMessage = '';
            
            IF @SCHEMA_OWNER IS NULL
-               SET @fullTableName = N'DBO.' + @TABLENAME
+               SET @fullTableName = N'DBO.' + @TABLENAME ;
            ELSE 
-               SET @fullTableName = @SCHEMA_OWNER + '.' + @TABLENAME
+               SET @fullTableName = @SCHEMA_OWNER + '.' + @TABLENAME ;
                
-           PRINT 'BEFORE TRUNCATE TABLE : ' + @fullTableName
+           PRINT 'BEFORE TRUNCATE TABLE : ' + @fullTableName ;
            
-           SET @SQLSTRING = N'TRUNCATE TABLE ' + @fullTableName /* 'DBO.' + @TABLENAME */
+           SET @SQLSTRING = N'TRUNCATE TABLE ' + @fullTableName ; 
            
            IF NOT EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(@fullTableName) AND TYPE IN(N'U'))
            BEGIN
@@ -54,11 +54,14 @@ BEGIN
      END TRY
      
      BEGIN CATCH --In case of errors
+                                                                                                            
          SELECT @errNumber = ERROR_NUMBER()
                ,@errMessage = ISNULL(ERROR_MESSAGE(), 'ERRORS : ') + ' ERROR LINE NUMBER : ' + CAST(ERROR_LINE() AS VARCHAR(20)) + ' ERROR NUMBER : ' + CAST(@ERRnUMBER AS VARCHAR(12))
                
          PRINT ' ERROR OCCURED IN SP : ' + @objectName + ' WITH ERROR : + @errMessage
+         
          RAISEERROR(@errMessage, 16, 1)
+         
      END CATCH
      
      RETURN @errNumber
